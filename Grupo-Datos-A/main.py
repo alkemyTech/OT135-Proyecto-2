0c2b55efaebdea8bf766082eafa9fa827d1228a3
LaureanoEngulian <laureanoengulian@gmail.com>
2022-02-15 19:17:37 -0300

    log example

--- Grupo-Datos-A/main.py
+++ Grupo-Datos-A/main.py
@@ -6,4 +6,4 @@ import os
 LOGGING_CONF=os.path.join(os.path.dirname(__file__), 'logging.cfg')
 fileConfig(LOGGING_CONF)
 logger = logging.getLogger(__name__)
-#logger.info('hello world')
+logger.info('hello world')
