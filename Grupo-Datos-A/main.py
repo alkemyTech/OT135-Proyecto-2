1b9d02e304f4c304d8b702952e1e6eabb8f7b8ac
LaureanoEngulian <laureanoengulian@gmail.com>
2022-02-15 16:29:27 -0300

    logger config removed from main to a .cfg file

--- Grupo-Datos-A/main.py
+++ Grupo-Datos-A/main.py
@@ -1,33 +1,9 @@
+
 import logging
 from logging.config import fileConfig
-from logging.handlers import TimedRotatingFileHandler
+import os
 
-logging.basicConfig(level=logging.INFO,
-                    format=f"%(asctime)s_[%(levelname)s]_%(name)s_%(message)s",
-                    datefmt='%d-%m-%Y')
-
-
-def get_file_handler():
-    file_handler = TimedRotatingFileHandler(filename="weekly_rotating.log",
-                                            when="W0"
-                                            )
-    file_handler.setLevel(logging.INFO)
-    return file_handler
-
-
-def get_stream_handler():
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-    return stream_handler
-
-
-def get_logger(name):
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    logger.addHandler(get_file_handler())
-    logger.addHandler(get_stream_handler())
-    return logger
-
-
-logger = get_logger(__name__)
-logger.info('Ejemplo')
+LOGGING_CONF=os.path.join(os.path.dirname(__file__), 'logging.cfg')
+fileConfig(LOGGING_CONF)
+logger = logging.getLogger()
+logger.info('hello world')
