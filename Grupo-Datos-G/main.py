b6abb6ecf5a366809b00933b588e20371ff57bce
Luis David <ldavidts@gmail.com>
2022-02-16 23:10:38 -0400

    corrected time format, deleted testing logs, modified logger with name variable

--- Grupo-Datos-G/main.py
+++ Grupo-Datos-G/main.py
@@ -4,10 +4,6 @@ from os import path
 
 log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.ini')
 logging.config.fileConfig(log_file_path)
+logger = logging.getLogger(__name__)
 
-logger = logging.getLogger('__name__')
 
-
-logger.info('This is info')
-logger.warning('This is warning')
-logger.error('This is error')
