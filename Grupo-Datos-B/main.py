7dde7a34e6beeaf0bcda51efa60a850b1cfdd633
nvrancovich <nvrancovich@gmail.com>
2022-02-17 08:24:18 -0300

    Add logging configuration

--- Grupo-Datos-B/main.py
+++ Grupo-Datos-B/main.py
@@ -2,7 +2,5 @@ import logging
 from logging.config import fileConfig
 import os
 
-LOGGING_CONF=os.path.join(os.path.dirname(__file__), 'logging.cfg')
-fileConfig(LOGGING_CONF)
+fileConfig(os.path.join(os.path.dirname(__file__), 'logging.cfg'))
 logger = logging.getLogger(__name__)
-logger.info('hello world')
