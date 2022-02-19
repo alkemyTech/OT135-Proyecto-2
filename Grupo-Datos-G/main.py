aef1462613c7beb143f4dddc8fa4acfd58822f72
Laureano Engulian <laureanoengulian@gmail.com>
2022-02-19 14:49:25 -0300

    mapped working

--- Grupo-Datos-G/main.py
+++ Grupo-Datos-G/main.py
@@ -1,42 +1,41 @@
 import logging
 from logging import config
 from os import path
-
 from functools import reduce
 import xml.etree.ElementTree as ET
+from numpy import iterable
 
 log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.ini')
 logging.config.fileConfig(log_file_path)
 logger = logging.getLogger(__name__)
 
-file_path = '/home/lengulian/Escritorio/Prueba_python/Stack Overflow 11-2010/112010 Meta Stack Overflow/posts.xml'
+file_path = '/home/lengulian/OT135-Proyecto-2/Grupo-Datos-G/posts.xml'
 tree = ET.parse(file_path)
 root = tree.getroot()
 
 # Top 10 posts mas vistos
-top_posts = []
+def chunkify(iterable, len_of_chunk):
+    for i in range(0, len(iterable), len_of_chunk):
+        yield iterable[i:i + len_of_chunk]
 
-
-def mapper():
-    for i in root:
-        ids = [int(i.attrib['Id']), int(i.attrib['ViewCount'])]
-        top_posts.append(ids)
-
-
-def reducer():
-    # mapper()
-    top_ten_posts = tuple(
-        sorted(top_posts, key=lambda item: item[1], reverse=True))[:10]
-    print(top_ten_posts)
-
-# Top 10 posts mas vistos
-def mapper(data):
+def obtener_id_views(data):
     return int(data.attrib['Id']), int(data.attrib['ViewCount'])
 
-def reducer(top_post):
-    return tuple(sorted(top_post, key=lambda item: item[1], reverse=True))
+def sort_best_10(posts):
+    sorted_posts = sorted(posts, key=lambda item: item[1], reverse=True)[:10]
+    return sorted_posts
+
+def mapper(top_posts):
+    posts = list(map(obtener_id_views, top_posts))
+    posts = sort_best_10(posts)
+    return posts
+
+#def reducer(posts1, posts2):
+    posts1 = posts1.extend(posts2)
+    print(posts1)
 
 
-mapped = list(map(mapper, root))[:20]
+data_chunks = chunkify(root, 50)
+mapped = list(map(mapper, data_chunks))
 #reduced = reduce(reducer, mapped)
 print(mapped)
