dce168c09b795da8b8cebd071ba094d46bc1d071
aguyanzon <aguyanzon@hotmail.com>
2022-02-21 12:47:44 -0300

    add docstring in functions

--- Grupo-Datos-F/main.py
+++ Grupo-Datos-F/main.py
@@ -30,10 +30,28 @@ except Exception as err:
 root = tree.getroot()
 
 def chunkify(iter, len_chunk):
+    """Itera desde un cierto rango hasta el total del tamaño del chunk
+
+    Args:
+        iter (xml): Árbol de elemento raíz del archivo xml.
+        len_chunk (int): Longitud del chunk
+
+    Yields:
+        xml: Devuelve un chunk de una determinada longitud
+    """
     for i in range(0, len(iter), len_chunk):
         yield iter[i:i + len_chunk]
 
 def reducer(cnt1, cnt2):
+    """Compara elementos de una secuencia.
+
+    Args:
+        cnt1 (tuple): Primer elemento a comparar
+        cnt2 (tuple: Segundo elemento a comparar
+
+    Returns:
+        tuple: Elemento actualizado.
+    """
     cnt1.update(cnt2)
     return cnt1
 
@@ -46,6 +64,11 @@ def mapper_dates(dates):
     return Counter(mapped_dates)
 
 def dates_with_fewer_posts():
+    """Realiza un map-reduce de los posts creados en cada fecha.
+
+    Returns:
+        list: Devuelve una lista de tuplas con las 10 fechas con menos posts creados.
+    """
     data_chunks = chunkify(root, 50)
     mapped = list(map(mapper_dates, data_chunks))
     reduced = reduce(reducer, mapped)
@@ -69,6 +92,11 @@ def mapper_words(data):
     return Counter(flatten_data)
 
 def top_ten_words_in_posts():
+    """Realiza un map-reduce de las palabras nombradas en los posts.
+
+    Returns:
+       list: Devuelve una lista de tuplas con las 10 palabras más nombradas en los posts..
+    """
     data_chunks = chunkify(root, 50)
     mapped = list(map(mapper_words, data_chunks))
     reduced = reduce(reducer, mapped)
