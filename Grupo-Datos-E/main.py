14e39d7ee4761b3263ea42250159bc686bf3b8fb
Juan Fernandez <juanfernandez_carc@hotmail.com>
2022-02-21 13:16:31 -0300

    Implementation of the function that returns the relationship between the number of responses and their visits

--- Grupo-Datos-E/main.py
+++ Grupo-Datos-E/main.py
@@ -1,7 +1,7 @@
 import logging
 from logging.config import fileConfig
 import os
-from map_functions import top_10
+from map_functions import top_10_posts, answers_visits
 
 LOGGING_CONF=os.path.join(os.path.dirname(__file__),
                           'logging.ini')
--- Grupo-Datos-E/map_functions.py
+++ Grupo-Datos-E/map_functions.py
@@ -5,10 +5,16 @@ from functools import reduce
 '''
 Script que realiza 3 funciones:
 - Top 10 fechas con mayor cantidad de post creados 
-- Relación entre cantidad de respuestas y sus visitas
-(por cada respuesta cuantas visitas)
+- Relación entre cantidad de respuestas y sus visitas - Answer -> ViewCount
 - Tiempo de respuesta promedio en top 0-100 post con mayor puntaje 
 '''
+
+# Devuelve un objeto con los datos cargados
+def routing():
+  route_file = 'posts.xml'
+  tree = ET.parse(route_file)
+  root = tree.getroot()
+  return root
 
 '''
 Itera desde el rango hasta el total del tamaño del chunk
@@ -25,29 +31,58 @@ def search_fecha(data):
   return data.attrib['CreationDate'][:10]
 
 # Devuelve una lista con las cantidades de post por fecha
-def mapper(fechas):
+def mapper_top(fechas):
   mapp_fechas = list(map(search_fecha, fechas))
   return Counter(mapp_fechas)
 
 # Aplica la funcion a 2 elementos y actualiza el resultado
-def reducer(cnt1, cnt2):
+def reducer_top(cnt1, cnt2):
   cnt1.update(cnt2)
   return cnt1
 
-# Devuelve un objeto con los datos cargados
-def routing():
-  route_file = 'posts.xml'
-  tree = ET.parse(route_file)
-  root = tree.getroot()
-  return root
-
 '''
-Funcion que devuelve el top 10 fechas con mayor cantidad de post creados
+Devuelve el top 10 fechas con mayor cantidad de post creados
 '''
-def top_10():
+def top_10_posts():
   # Ruta del archivo .xml
   root = routing()
   data_chunks = chukinfy(root, 50)
-  mapped = list(map(mapper, data_chunks))
-  reduced = reduce(reducer, mapped)
-  return reduced.most_common(10)
+  mapped_top = list(map(mapper_top, data_chunks))
+  reduced_top = reduce(reducer_top, mapped_top)
+  return reduced_top.most_common(10)
+
+'''
+Recibe un elemento y devuelve el id y la cantidad de visitas
+Si es PostTypeId = 1 es question, devuelve None
+'''
+def id_views(data):
+  if data.attrib['PostTypeId'] == '2':
+    return int(data.attrib['Id']), int(data.attrib['ViewCount'])
+  else:
+    pass
+
+def order_top(asnwers):
+  ordered = sorted(asnwers, key=lambda item: item[1])
+  return ordered
+
+def reducer_answers(cp1, cp2):
+    cp1 = cp1 + cp2
+    cp2 = order_top(cp1)
+    return cp1
+
+def mapper_answers(top_posts):
+  mapp_answers = list(map(id_views, top_posts))
+  # Filtra las question (None)
+  mapp_answers = list(filter(None, mapp_answers))
+  return mapp_answers
+
+'''
+Devuelve la relación entre cantidad de respuestas y sus visitas
+(por cada respuesta cuantas visitas)
+'''
+def answers_visits():
+  root = routing()
+  data_chunks = chukinfy(root, 50)
+  mapped = list(map(mapper_answers, data_chunks))
+  reduced = reduce(reducer_answers, mapped)
+  return reduced
