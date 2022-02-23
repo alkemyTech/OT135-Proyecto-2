66d1d73c784c482bffc36ba2199286f916306d23
nvrancovich <nvrancovich@gmail.com>
2022-02-23 09:57:33 -0300

    Add first query top 10 tags with most accepted answers

---
+++ Grupo-Datos-A/query1.py
@@ -0,0 +1,61 @@
+import xml.etree.ElementTree as ET
+import functools
+from functools import reduce
+from itertools import chain
+from bs4 import BeautifulSoup
+import re
+from collections import Counter
+import os
+
+mytree = ET.parse('Grupo-Datos-A/112010 Meta Stack Overflow/posts.xml')
+myroot = mytree.getroot()
+
+def extract_tags(data):
+    yield data.attrib['Tags']
+
+def extract_answers(data):
+    yield data.attrib['AnswerCount']
+
+def clean_tags(tags):
+    tags_list = []
+    for tag in tags:
+        try:
+            tag = next(tag)
+            tag = tag.replace('<','')
+            tag = tag.replace('>',' ')
+            tag = tag.split()
+            tags_list.append(tag)
+        except:
+            tags_list.append(None)
+    return tags_list
+
+def filter_answers(answers):
+    answers_filtered = []
+    for answer in answer_count:
+        try:
+            answer = int(next(answer))
+            answers_filtered.append(answer)
+        except:
+            answers_filtered.append(0)
+    return answers_filtered
+
+tags = list(map(extract_tags,myroot))
+tags = clean_tags(tags)
+
+answer_count = list(map(extract_answers,myroot))
+answer_count = filter_answers(answer_count)
+
+tags_answers = []
+for tag_list in tags:
+    if (tag_list is not None):
+        for tag in tag_list:
+            tags_answers.append((tag, answer_count[tags.index(tag_list)]))
+
+results = {}
+for tag_answer in tags_answers:
+    if tag_answer[0] in results:
+        results[tag_answer[0]] += tag_answer[1]
+    else:
+        results[tag_answer[0]] = tag_answer[1]
+    
+print(sorted(results, key=results.get, reverse=True)[:10])
