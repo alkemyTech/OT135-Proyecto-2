a94993dc3d05c6d32592984667fae71fe403069e
aguyanzon <aguyanzon@hotmail.com>
2022-02-24 16:43:17 -0300

    add functions to calculate average time in top 100-200

--- Grupo-Datos-F/main.py
+++ Grupo-Datos-F/main.py
@@ -1,10 +1,11 @@
-from collections import Counter
-from functools import reduce
 import logging as log
-from logging.config import fileConfig
 import os
 import re
 import xml.etree.ElementTree as ET
+from collections import Counter
+from datetime import datetime
+from functools import reduce
+from logging.config import fileConfig
 
 config = os.path.join(os.path.dirname(__file__), 'logging.cfg')
 fileConfig(config)
@@ -93,7 +94,102 @@ def top_ten_words_in_posts():
     reduced = reduce(reducer, mapped)
     return reduced.most_common(10)
 
+# Tiempo de respuesta promedio en top 100-200 score
+def id_score_dates(data):
+    post_id = data.attrib['PostTypeId']
+    score = int(data.attrib['Score'])
+    id = data.attrib['Id']
+    creation_date = data.attrib['CreationDate']
+    if post_id == '1':
+        return id, score, creation_date
+    else:
+        pass
+
+def parentid_dates(data):
+    post_id = data.attrib['PostTypeId']
+    creation_date = data.attrib['CreationDate']
+    #parent_id = data.attrib['ParentId']
+    if post_id == '2':
+        parent_id = data.attrib['ParentId']
+        return parent_id, creation_date
+    else:
+        pass
+
+def format_date_1(data):
+    return data[0], data[1], datetime.strptime(data[2], '%Y-%m-%dT%H:%M:%S.%f')
+
+def format_date_2(data):
+    return data[0], datetime.strptime(data[1], '%Y-%m-%dT%H:%M:%S.%f')
+
+def reducer_1(data1, data2):
+    data1 = data1 + data2
+    return data1
+
+def mapper_1(data):
+    post_type_1 = list(map(id_score_dates, data))
+    post_type_1 = list(filter(None, post_type_1))
+    post_type_1 = list(map(format_date_1, post_type_1))
+    return post_type_1
+
+def mapper_2(data):
+    post_type_2 = list(map(parentid_dates, data))
+    post_type_2 = list(filter(None, post_type_2))
+    post_type_2 = list(map(format_date_2, post_type_2))
+    return post_type_2
+
+def reducer2(data1, data2):
+    """
+    Obtiene los tiempos de respuesta comparando los id de las preguntas 
+    con el parent id de las respuestas.
+    """
+    for elem in type1_reduced:
+        for i in data1:
+            while elem[0] == i[0]:
+                time = i[1] - elem[2]
+                times.append(time)
+                break
+    data1 = data2
+    return data1
+
+def time_reducer(time1, time2):
+    """Suma todos los tiempos de respuesta en uno
+
+    Args:
+        time1 (datetime): Fecha
+        time2 (datetime): Fecha
+
+    Returns:
+        datetime: Fecha
+    """
+    time1 = time1 + time2
+    return time1
+
+def avg_time_100_200():
+    """
+    Realiza un map-reduce para calcular el tiempo promedio de respuesta 
+    en el top 100-200 score.
+
+    Returns:
+        str: Tiempo promedio de respuesta.
+    """
+    global type1_reduced, times
+    data_chunks = chunkify(root, 50)
+    mapped_1 = list(map(mapper_1, data_chunks))
+    type1_reduced = reduce(reducer_1, mapped_1)
+    type1_reduced = sorted(type1_reduced, key=lambda x: x[1], reverse=True)[100:200]
+
+    data_chunks = chunkify(root, 50)
+    mapped_2 = list(map(mapper_2, data_chunks))
+
+    times = []
+    reduce(reducer2, mapped_2)
+
+    avg_time_100_200 = reduce(time_reducer, times)/len(times)
+    return avg_time_100_200
+
+
 if __name__ == '__main__':
     print('Top 10 fechas con menor cantidad de post creados:\n', dates_with_fewer_posts())
     print('Top 10 palabras mas nombradas en los posts:\n', top_ten_words_in_posts())
+    print('Tiempo de respuesta promedio en top 100-200 score:\n', avg_time_100_200())
 
