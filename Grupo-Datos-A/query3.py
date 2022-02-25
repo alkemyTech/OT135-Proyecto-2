69d333f96867ddf66949ac0d90a4237a44c55f3e
nvrancovich <nvrancovich@gmail.com>
2022-02-24 21:45:36 -0300

    Fix query3 format and add doc

--- Grupo-Datos-A/query3.py
+++ Grupo-Datos-A/query3.py
@@ -1,4 +1,10 @@
 from datetime import datetime
+from itertools import chain
+from statistics import mean
+
+def chunkify(iterable, len_of_chunk):
+    for i in range(0, len(iterable), len_of_chunk):
+        yield iterable[i:i + len_of_chunk]
 
 def extract_CreationDate(root):
     return root.attrib['CreationDate']
@@ -15,29 +21,48 @@ def toDatetime(string):
     date = datetime.strptime(string,'%Y-%m-%dT%H:%M:%S.%f')
     return date
     
-def daleay(CreationDates, LastEditDates):
-    delayTime = 0
+def delay(CreationDates, LastEditDates):
+    delayTimes = []
     for date in LastEditDates:
-        delay = (date - CreationDates[LastEditDates.index(date)]).total_seconds()
-        delayTime = (delayTime+delay)/2
-    return delayTime
+        delayTimes.append((date - CreationDates[LastEditDates.index(date)]).total_seconds())
+    return delayTimes
 
-def query3(myroot):
+def mapper(myroot):
+    
     creationDates = list(map(extract_CreationDate, myroot))
-
+    
     lastEditDates = list(map(extract_LastEditDate, myroot))
 
-    for i in range(10):
-        for date in creationDates:
-            index = creationDates.index(date)
-            if lastEditDates[index] is None:
-                del lastEditDates[index]
-                del creationDates[index]
+    filteredCreationDates = []
+    
+    filteredLastEditDates = []
 
-    creationDates = list(map(toDatetime,creationDates))
+    for date in creationDates:
+        index = creationDates.index(date)
+        if lastEditDates[index] is not None:
+            filteredCreationDates.append(creationDates[index])
+            filteredLastEditDates.append(lastEditDates[index])
+        
+    del creationDates
+    del lastEditDates
 
-    lastEditDates = list(map(toDatetime,lastEditDates))
+    filteredCreationDates = list(map(toDatetime,filteredCreationDates))
 
-    delayTime = daleay(creationDates, lastEditDates)
+    filteredLastEditDates = list(map(toDatetime,filteredLastEditDates))
 
+    delayTime = delay(filteredCreationDates, filteredLastEditDates)
+    
     return delayTime
+
+
+
+def query3(myroot):
+    '''
+    Esta función recibe como parametro una root de un archivo xml parseado con ElementTree.
+    Aplica técnicas de map-reduce y devuelve el tiempo promedio de respuesta en días.
+    '''
+    myroot = chunkify(myroot, 100)
+    mapped = list(map(mapper, myroot))
+    reduced = mean(list(chain(*mapped)))
+
+    return ((reduced/60)/60)/24
